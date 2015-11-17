9d9c77d05e4f2ec75d04f2f60377c17ebf23c520
Spencer Smith <spesmit@siue.edu>
2015-11-17 14:27:38 -0600

    added commits to stucture object

--- Hotspotter/client/fileView/fileViewService.js
+++ Hotspotter/client/fileView/fileViewService.js
@@ -7,21 +7,21 @@
 		var treeData = {folders: [], files: []};
         var tree = treeData;
         // loop through file paths
-		angular.forEach(list, function(value) {
+		for (var i = 0, len_i=list.length; i < len_i; i++) {
 			// Split file paths into array and loop through 
-			var pathSplit = value.Name.replace(/\//g,'/,').split(/,/);
-			angular.forEach(pathSplit, function(value) {
+			var pathSplit = list[i].Name.replace(/\//g,'/,').split(/,/);
+			for (var j = 0, len_j=pathSplit.length; j < len_j; j++) {
                 // ignore '/' root directory
-                if (value != '/') {
+                if (pathSplit[j] != '/') {
                     // insert file name
-                    if (value.indexOf('/') < 0) {
-                        tree.files.push({name: value});
+                    if (pathSplit[j].indexOf('/') < 0) {
+                        tree.files.push({name: pathSplit[j], commits: list[i].Commits});
                     // insert directory name
                     } else {
                         // find next directory in path
                         var found = 0;
                         for(var index in tree.folders) {
-                            if (tree.folders[index].name == value) {
+                            if (tree.folders[index].name == pathSplit[j]) {
                                 tree = tree.folders[index];
                                 found = 1;
                                 break;
@@ -29,14 +29,14 @@
                         }
                         // directory doesn't exists so create folder object
                         if (found === 0) {
-                            tree.folders.push({name: value, folders: [], files: []});
+                            tree.folders.push({name: pathSplit[j], folders: [], files: []});
                             tree = tree.folders[tree.folders.length-1];
                         }  
                     }    
                 }      
-			}, treeData);
+			}
         tree = treeData;
-		}, treeData);
+		}
 	return treeData;
 	};
 
