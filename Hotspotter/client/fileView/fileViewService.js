41163a058d68c67fc16e13721812e296535152e8
Spencer Smith <spesmit@siue.edu>
2015-11-17 17:00:55 -0600

    added commit max and min

--- Hotspotter/client/fileView/fileViewService.js
+++ Hotspotter/client/fileView/fileViewService.js
@@ -4,10 +4,23 @@
 	ngModule.service('fileViewService', function () {
 	this.ParsingAPI = function(list) {
         console.log(list);
+
+        var Max = 0;
+        var Min = Number.MAX_VALUE;
+        for (var i = 0, len_i=list.length; i < len_i; i++) {
+            if (list[i].Commits > Max) {
+                Max=list[i].Commits;
+            }
+            if (list[i].Commits < Min) {
+                Min=list[i].Commits;
+            }
+
+        }
+
 		var treeData = {folders: [], files: []};
         var tree = treeData;
         // loop through file paths
-		for (var i = 0, len_i=list.length; i < len_i; i++) {
+		for (i = 0, len_i=list.length; i < len_i; i++) {
 			// Split file paths into array and loop through 
 			var pathSplit = list[i].Name.replace(/\//g,'/,').split(/,/);
 			for (var j = 0, len_j=pathSplit.length; j < len_j; j++) {
@@ -15,7 +28,7 @@
                 if (pathSplit[j] != '/') {
                     // insert file name
                     if (pathSplit[j].indexOf('/') < 0) {
-                        tree.files.push({name: pathSplit[j], commits: list[i].Commits});
+                        tree.files.push({name: pathSplit[j], score: (1-((list[i].Commits-Min)/(Max-Min)))});
                     // insert directory name
                     } else {
                         // find next directory in path
