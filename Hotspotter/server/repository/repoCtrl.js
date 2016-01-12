1cb6aa6edf510a44cb90d02d2751b490fe56509c
Spencer Smith <spesmit@siue.edu>
2016-01-12 13:13:38 -0600

    moved createTree function to repoService

--- Hotspotter/server/repository/repoCtrl.js
+++ Hotspotter/server/repository/repoCtrl.js
@@ -1,5 +1,6 @@
 var Repo = require('./repoModel');
 var gitService = require('../git/gitService');
+var repoService = require('./repoService');
 var Glob = require('glob').Glob;
 var File = require('../file/fileModel');
 var async = async = require("async");
@@ -26,67 +27,8 @@ module.exports.view = function (req, res) {
             res.json([]);
         } else {
             gitService.gitLogCommits(filePaths, function (files) {
-                //console.log(files);
-                var Max = 0;
-                var Min = Number.MAX_VALUE;
-                
-                async.each(files, function (file, callback) {
-                    if (file.Commits > Max) {
-                        Max=file.Commits;
-                    }
-                    if (file.Commits < Min) {
-                        Min=file.Commits;
-                    }
-                    callback();
-                },
-                function (err) {
-                    var treeData = {folders: [], files: []};
-                    var tree = treeData;
-                    async.each(files, function (filePaths, callback1) {
-                        var pathSplit = filePaths.Name.replace(/\//g,'/,').split(/,/);
-                        async.each(pathSplit, function (path, callback2) {
-                             // ignore '/' root directory
-                            //console.log(filePaths);
-                            if (path != '/') {
-                                // insert file name
-                                if (path.indexOf('/') < 0) {
-                                    tree.files.push({name: path, score: (1-((filePaths.Commits-Min)/(Max-Min)))});
-                                // insert directory name
-                                } else {
-                                // find next directory in path
-                                //console.log(tree);
-                                async.each(tree.folders, function (folder, callback3) {
-                                    if (folder.name == path) {
-                                        tree = folder;
-                                        var found = new Error();
-                                        found.break = true;
-                                        return callback3(found);
-                                    } 
-                                    callback3();
-                                },
-                                function (err) {
-                                    // directory doesn't exists so create folder object
-                                    if (err && err.break) {
-                                        // do nothing
-                                    } else {
-                                        tree.folders.push({name: path, folders: [], files: []});
-                                        tree = tree.folders[tree.folders.length-1];
-                                    }
-                                });
-                                }
-                            }
-                            callback2();
-                        },
-                        function (err) {
-                            tree = treeData;
-                        });
-                    callback1();
-
-                    },
-                    function (err) {
-                        res.json(treeData)
-                    });
-                
+                repoService.createTree(files, function (tree) {
+                    res.json(tree);
                 });
             });
         }
---
+++ Hotspotter/server/repository/repoService.js
@@ -0,0 +1,71 @@
+/**
+ * Created by SmithS on 01/12/2016.
+ */
+
+var File = require('../file/fileModel');
+var async = require("async");
+
+exports.createTree = function (files, res) {
+    //console.log(files);
+	var Max = 0;
+    var Min = Number.MAX_VALUE;
+    
+    async.each(files, function (file, callback) {
+        if (file.Commits > Max) {
+            Max=file.Commits;
+        }
+        if (file.Commits < Min) {
+            Min=file.Commits;
+        }
+        callback();
+    },
+    function (err) {
+        var treeData = {folders: [], files: []};
+        var tree = treeData;
+        async.each(files, function (filePaths, callback1) {
+            var pathSplit = filePaths.Name.replace(/\//g,'/,').split(/,/);
+            async.each(pathSplit, function (path, callback2) {
+                 // ignore '/' root directory
+                //console.log(filePaths);
+                if (path != '/') {
+                    // insert file name
+                    if (path.indexOf('/') < 0) {
+                        tree.files.push({name: path, score: (1-((filePaths.Commits-Min)/(Max-Min)))});
+                    // insert directory name
+                    } else {
+                    // find next directory in path
+                    //console.log(tree);
+                    async.each(tree.folders, function (folder, callback3) {
+                        if (folder.name == path) {
+                            tree = folder;
+                            var found = new Error();
+                            found.break = true;
+                            return callback3(found);
+                        } 
+                        callback3();
+                    },
+                    function (err) {
+                        // directory doesn't exists so create folder object
+                        if (err && err.break) {
+                            // do nothing
+                        } else {
+                            tree.folders.push({name: path, folders: [], files: []});
+                            tree = tree.folders[tree.folders.length-1];
+                        }
+                    });
+                    }
+                }
+                callback2();
+            },
+            function (err) {
+                tree = treeData;
+            });
+        callback1();
+
+        },
+        function (err) {
+            res(treeData);
+        });
+    
+    });
+};
