e4276fc1a0bd1c0a37e47568400fb5c536f1965f
Intern Nate <nate.reinhardt@wwt.com>
2016-01-13 14:38:40 -0600

    refactored dashboard controller and view

--- Hotspotter/client/dashboard/dashboardCtrl.js
+++ Hotspotter/client/dashboard/dashboardCtrl.js
@@ -2,26 +2,48 @@
     'use strict';
     var ngModule = angular.module('hotspotter.dashboardCtrl', []);
     ngModule.controller('dashboardCtrl', function ($scope, $resource) {
-    	var Repo = $resource("/api/repo");
 
-    	$scope.success 		= false;
-        $scope.repos = [];
-    	$scope.addRepository = function() {
-    		// Create new repository object and save URL
-    		var repo = new Repo();
-    		repo.URL = $scope.repoUrl;
-                repo.$save(function (result){
-                    $scope.repos.push(result);
-                });
-    		// Update frontend display
-    		$scope.success = true;
-    		$scope.added_repo = $scope.repoUrl;
-    		$scope.repoUrl = '';
-    	};
-        // Log debug
-        Repo.query(function (results){
-            $scope.repos = results;
-        });
+        //"Global Variables"
+        var Repo = $resource("/api/repo");
+
+
+        var vm = this;
+        vm.success = false;
+        vm.repos = [];
+
+        //"Global Functions"
+        vm.addRepository = addRepository;
+
+
+        init();
+
+        //Anything that needs to be instantiated on page load goes in the init
+        function init() {
+            listRepos();
+        }
+
+        //Adds checkouts a repository
+        function addRepository() {
+
+            var repo = new Repo();
+
+            repo.URL = vm.repoUrl;
+
+            //Save Repo to Database
+            repo.$save(function (result) {
+                vm.repos.push(result);
+            });
+
+            // Update frontend display
+            vm.success = true;
+            vm.addedRepo = vm.repoUrl;
+            vm.repoUrl = '';
+        }
+
+        //Lists all repos that have been checked out
+        function listRepos() {
+            vm.repos = Repo.query();
+        }
 
     });
 }(window.angular));
