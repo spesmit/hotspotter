89db581dc8228698195b0ddc91f84452bdfb0b6e
Dylan <dywilli@siue.edu>
2016-01-17 17:48:34 -0600

    Repos are cloned into a subfolder of tempProjects with a name equivalent to the sha1 hash of their URL

--- Hotspotter/server/git/gitService.js
+++ Hotspotter/server/git/gitService.js
@@ -2,19 +2,26 @@
  * Created by natem on 10/25/2015.
  */
 var localPath = ('./tempProjects');
-var File = require('../file/fileModel');
-var async = require("async");
+var File      = require('../file/fileModel');
+var async     = require("async");
+var crypto    = require("crypto");
+var sha1      = function(input) {
+  return crypto.createHash('sha1').update(JSON.stringify(input)).digest('hex');
+}
 
 
 exports.gitCheckout = function (repoURL){
     var simpleGitClone = require('simple-git')();
+    var repoURLHash = sha1(repoURL);
+    var repoPath = localPath + "/" + repoURLHash;
     console.log("REPO URL: " + repoURL);
+    console.log("REPO HASH: " + repoURLHash);
     simpleGitClone
         .outputHandler(function (command, stdout, stderr) {
             stdout.pipe(process.stdout);
             stderr.pipe(process.stderr);
         })
-        .clone(repoURL,localPath);
+        .clone(repoURL, repoPath);
 };
 
 exports.gitLogCommits = function (filePaths, res) {
