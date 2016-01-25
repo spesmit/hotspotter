c4ccf1ca2ed4ff7ed4b98e74da5beae3b7eedf24
natereinhardt <natemreinhardt@gmail.com>
2016-01-24 23:51:39 -0600

    still tinkering with testing, injection problems

--- Hotspotter/client/admin/adminCtrlSpec.js
+++ Hotspotter/client/admin/adminCtrlSpec.js
@@ -1,15 +1,28 @@
-describe('adminCtrl', function(){
-    beforeEach(
-        module('hotspotter'),
-        module('hotspotter.adminCtrl'));
+'use strict';
+describe('hotspotter.adminCtrl', function(){
+    var adminCtrl,
+        $httpBackend,
+        $resource,
+        scope;
+
+
+    //mock Application to allow us to inject our own dependencies
+    beforeEach(angular.mock.module('hotspotter'));
+
+    beforeEach(angular.mock.inject(function($rootScope, $controller, _$httpBackend_){
+        $httpBackend = _$httpBackend_;
+        $httpBackend.when('GET', 'Users/users.json').respond([{id: 1, name: 'Bob'}, {id:2, name: 'Jane'}]);
+
+        //create an empty scope
+        scope = $rootScope.$new();
+        //declare the controller and inject our empty scope
+        adminCtrl = $controller('adminCtrl', {$scope: scope});
+    }));
 
     describe('listRepos()', function(){
-        it('should list the repos', inject(function($controller){
-            var scope = {};
-            var adminCtrl = $controller('adminCtrl' ,{
-                $scope: scope
-            });
+        it('should list the repos', function (){
             console.log("hello world")
-        }));
+
+        });
     });
 });
