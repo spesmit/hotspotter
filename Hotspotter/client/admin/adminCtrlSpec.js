7319eb7d438a20167c2f662a0b3461095f012ac9
Intern Nate <nate.reinhardt@wwt.com>
2016-01-26 12:36:26 -0600

    where i left off 01-25'

--- Hotspotter/client/admin/adminCtrlSpec.js
+++ Hotspotter/client/admin/adminCtrlSpec.js
@@ -1,28 +1,71 @@
 'use strict';
-describe('hotspotter.adminCtrl', function(){
-    var adminCtrl,
-        $httpBackend,
-        $resource,
-        scope;
+
+describe('adminCtrl', function () {
+
+    var $mockBackend;
+    var adminCtrl;
+    var scope;
+
+    var expectedResponse = {id: 12345, url: "https://github.com/myuser/myrepo.git"};
 
 
-    //mock Application to allow us to inject our own dependencies
-    beforeEach(angular.mock.module('hotspotter'));
+    beforeEach(function () {
+        module('hotspotter.adminCtrl');
+        module('hotspotter');
 
-    beforeEach(angular.mock.inject(function($rootScope, $controller, _$httpBackend_){
-        $httpBackend = _$httpBackend_;
-        $httpBackend.when('GET', 'Users/users.json').respond([{id: 1, name: 'Bob'}, {id:2, name: 'Jane'}]);
+    });
 
-        //create an empty scope
-        scope = $rootScope.$new();
-        //declare the controller and inject our empty scope
+    beforeEach(inject(function ($injector) {
+        $mockBackend = $injector.get($httpBackend);
+        scope = $injector.get($rootScope).$new();
+        var $controller = $injector.get('adminCtrl');
+
+        $mockBackend.when('/api/repo').respond([expectedResponse]);
+        $mockBackend.when('/api/file').respond([expectedResponse]);
+        $mockBackend.when('/api/repo').respond([expectedResponse]);
+        $mockBackend.when('/api/repo').respond([expectedResponse]);
+
         adminCtrl = $controller('adminCtrl', {$scope: scope});
-    }));
 
-    describe('listRepos()', function(){
-        it('should list the repos', function (){
-            console.log("hello world")
+    }));
+        it('should return a json object representing a repository', function () {
+            adminCtrl.listRepos();
+
+
+
+            $mockBackend.flush();
+
+            expect(adminCtrl.repos.length).toEqual(1);
+            console.log(adminCtrl.repos[0]);
+            expect((angular.equals(adminCtrl.repos[0], expectedResponse)));
+
 
         });
     });
-});
+
+
+
+//describe('listFiles()', function () {
+//    it('should return a json object representing the files of a repository',
+//        inject(function (_$httpBackend_, $rootScope, $controller) {
+//
+//            var scope = $rootScope.$new();
+//            var mockBackend = _$httpBackend_;
+//
+//            var requestedURL = "https://github.com/myuser/myrepo.git";
+//            var expectedResponse = {id: 12345, url: requestedURL, file: 'index.html'};
+//
+//            mockBackend.expectGET('/api/file/' + requestedURL).respond([expectedResponse]);
+//
+//            var ctrl = $controller('adminCtrl', {$scope: scope});
+//            ctrl.listFiles(requestedURL);
+//
+//            mockBackend.flush();
+//
+//            expect(ctrl.files.length).toEqual(1);
+//            console.log(ctrl.files[0]);
+//            expect((angular.equals(ctrl.files[0], expectedResponse)));
+//        }));
+//});
+
+
