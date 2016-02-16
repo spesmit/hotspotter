e7b4c3009f59ca3854196fafe1698f19c27206e8
Spencer Smith <spesmit@siue.edu>
2016-02-15 22:53:23 -0600

    updated grunt file

--- Hotspotter/Gruntfile.js
+++ Hotspotter/Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function(grunt) {
             CLIENT_DIR + "/dashboard/*.js",
             CLIENT_DIR + "/fileView/*.js",
             CLIENT_DIR + "/admin/*.js",
+            CLIENT_DIR + "/scoring/*.js",
             CLIENT_DIR + "/thirdParty/angular-tree-view/js/treeView.js",
             "!" + CLIENT_DIR + "/**/*Spec.js"
         ];
